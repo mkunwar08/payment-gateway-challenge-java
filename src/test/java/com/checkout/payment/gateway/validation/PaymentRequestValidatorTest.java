f2499b9de1393ff4b50d466d2fd106e23b0570bd
Manish Kunwar <manish@Manishs-Laptop.local>
2026-03-19 22:52:57 +0000

    Add additional unit tests for validator class

--- src/test/java/com/checkout/payment/gateway/validation/PaymentRequestValidatorTest.java
+++ src/test/java/com/checkout/payment/gateway/validation/PaymentRequestValidatorTest.java
@@ -3,6 +3,9 @@ package com.checkout.payment.gateway.validation;
 import com.checkout.payment.gateway.model.PostPaymentRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -33,6 +36,7 @@ public class PaymentRequestValidatorTest {
     assertTrue(validator.isValid(getValidRequest()));
   }
 
+  //tests for card number
   @Test
   void lessThanFourteenDigitsCardNoFailsValidation() {
     PostPaymentRequest request = getValidRequest();
@@ -68,4 +72,39 @@ public class PaymentRequestValidatorTest {
     assertFalse(validator.validateCardNumber(cardNo).isEmpty());
     assertFalse(validator.isValid(request));
   }
+
+  //tests for expiry dates
+  @Test
+  void expiryDateInThePastFailsValidation() {
+    PostPaymentRequest request = getValidRequest();
+    request.setExpiryMonth(12);
+    request.setExpiryYear(2025);
+    assertFalse(validator.isValid(request));
+  }
+
+  //tests for currency
+  @ParameterizedTest
+  @ValueSource(strings = {"GBP", "EUR", "USD"})
+  void supportedCurrencyPassesValidation(String currency) {
+    PostPaymentRequest request = getValidRequest();
+    request.setCurrency(currency);
+    assertTrue(validator.isValid(request));
+  }
+
+  @Test
+  void unsupportedCurrencyFailsValidation() {
+    PostPaymentRequest request = getValidRequest();
+    request.setCurrency("YEN");
+    assertFalse(validator.isValid(request));
+  }
+
+  //tests for cvv
+  @ParameterizedTest
+  @ValueSource(strings = {"01234", "01", "xyz"})
+  public void invalidCvvFailsValidation(String cvv) {
+    PostPaymentRequest request = getValidRequest();
+    request.setCvv(cvv);
+    assertFalse(validator.isValid(request));
+  }
+
 }
