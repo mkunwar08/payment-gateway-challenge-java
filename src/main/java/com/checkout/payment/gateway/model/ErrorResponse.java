98fcda832d5138e41aebf1934d9e1fb97f1b6241
Manish Kunwar <manish@Manishs-Laptop.local>
2026-03-19 21:39:59 +0000

    Modify ErrorResponse class to surface errors to the request caller

--- src/main/java/com/checkout/payment/gateway/model/ErrorResponse.java
+++ src/main/java/com/checkout/payment/gateway/model/ErrorResponse.java
@@ -1,10 +1,30 @@
 package com.checkout.payment.gateway.model;
 
+import java.util.List;
+
 public class ErrorResponse {
+
   private final String message;
+  private final String status;
+  private final List<String> errors;
 
   public ErrorResponse(String message) {
     this.message = message;
+    this.status = null;
+    this.errors = null;
+  }
+
+  public ErrorResponse(String status, List<String> errors) {
+    this.status = status;
+    this.errors = errors;
+    this.message = null;
+  }
+  public String getStatus() {
+    return status;
+  }
+
+  public List<String> getErrors() {
+    return errors;
   }
 
   public String getMessage() {
